17cd44aa0df71422a09c95377cc8108d1b1eae57
Juan Manuel Valencia Zapata <jmvalenciz@protonmail.com>
2024-11-08 22:38:49 -0500

    First version

---
+++ src/main/java/com/nequi/franquicias/franquicias/FranquiciasManagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.nequi.franquicias.franquicias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FranquiciasManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FranquiciasManagerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nequi/franquicias/franquicias/FranquiciasManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nequi.franquicias.franquicias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FranquiciasManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
